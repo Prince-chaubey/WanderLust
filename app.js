508fb7d1cdaa66b4276c5ad14b89a9e3aba2987f
Kumar Ashish <princechaubey1895@gmail.com>
2025-04-04 23:48:34 +0530

    Performed Edit Route operation

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ const app=express();
 const mongoose=require("mongoose");
 const Listing=require("./Models/Listing.js");
 const path=require("path");
+const methodOverride = require('method-override');
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname,"public")));
+app.use(methodOverride('_method'));
 
 
 const port=8080;
-
-
 
 //Connected database 
 const URL="mongodb://127.0.0.1:27017/wanderLust";
@@ -35,6 +37,26 @@ app.get("/listings",async(req,res)=>{
    const allListings=await Listing.find({});
    res.render("index.ejs",{allListings});
 })
+
+//Rooute to edit the Listings
+app.put("/listings/:id",async(req,res)=>{
+    const id=req.params.id;
+    const {title,description,image,price,location,country}=req.body;
+    await Listing.findByIdAndUpdate(id,{title,description,image,price,location,country});
+    res.redirect("/listings");
+})
+
+//Route to render the edit form
+
+app.get("/listings/:id/edit",async(req,res)=>{
+    const List=await Listing.findById(req.params.id);
+    if(!List){
+        return res.status(404).send("Listing not found");
+    }
+    res.render("edit.ejs",{List});
+
+})
+
 //Create a new Route
 app.get("/listings/new",(req,res)=>{
     res.render("create.ejs");
@@ -50,8 +72,6 @@ app.post(("/listings/new"),async(req,res)=>{
     res.redirect("/listings");
 })
 
-
-
 //Show Route
 app.get("/listings/:id",async(req,res)=>{
     try {
@@ -66,7 +86,6 @@ app.get("/listings/:id",async(req,res)=>{
         res.status(400).send("Invalid Listing ID");
     }
 })
-
 
 //Listener on the port
 app.listen(port,()=>{
